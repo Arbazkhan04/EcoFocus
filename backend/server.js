8568ebcbf8cb4d4aa4cc12e876f2cd37ec2905df
Arbaz <arbaz582021@gmail.com>
2024-11-28 11:28:53 -0500

    reset password and email verification done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const connectDb = require('./db/connect.js')
+const cors = require('cors');
 
 const app = express();
 
 const PORT  = process.env.PORT || 5000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(express.raw());
+app.use(cors())
+
+
+//Start Block Accessing The Routes in the Entry Point
+const userManagementRoutes = require('./routes/userManagementRouter.js');
+
+
+//start using routes
+app.use('/api/userManagementRoutes',userManagementRoutes);
+//end usng routes
+
+
+//End Block Accessing The Routes in the Entry Point
 
 const start = async () => {
     try {
