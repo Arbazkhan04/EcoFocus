b99d94ef7a876794afbfd7715fa3dfc353710f16
Arbazkhan04 <arbaz582021@gmail.com>
2024-11-30 08:28:02 -0500

    schema done

---
+++ backend/modals/agencyManagementModal.js
@@ -0,0 +1,15 @@
+const requestSchema = new mongoose.Schema({
+    type: { 
+      type: String, 
+      enum: ['user_to_company', 'company_to_user', 'user_to_agency', 'company_to_agency'], 
+      required: true 
+    },
+    from: { type: mongoose.Schema.Types.ObjectId, required: true }, // User or Company ID
+    to: { type: mongoose.Schema.Types.ObjectId, required: true },   // User or Company ID
+    status: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model('Request', requestSchema);
+  
